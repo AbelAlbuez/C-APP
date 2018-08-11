87f324f6745919717152e0433d2291a2d4ea0713
JancerFabian <jancer.fabian@gmail.com>
2018-08-11 18:33:25 -0400

    loginTerminadoFull

--- application/views/action_user/login.php
+++ application/views/action_user/login.php
@@ -41,54 +41,44 @@ $submit = array(
 <section class="login-block">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4 login-sec">
+			<div class="col-md-12 login-sec">
 				<h2 class="text-center">Ingresa a tu cuenta</h2>
 
-	<?php echo form_open();?><br>
+				<?php echo form_open();?>
+				<br>
 
 
-<?php echo form_label('Correo Electronico o Username');?><br>
-<?php echo form_input($input_correo);?><br>
-<?php echo form_error('correo') ?><br>
+				<?php echo form_label('Correo Electronico o Username');?>
+				<br>
+				<?php echo form_input($input_correo);?>
+				<br>
+				<?php echo form_error('correo') ?>
+				<br>
 
-<?php echo form_label('Contraseña');?><br>
-<?php echo form_input($input_contrasenia);?><br>
-<?php echo form_error('contrasenia') ?><br>
+				<?php echo form_label('Contraseña');?>
+				<br>
+				<?php echo form_input($input_contrasenia);?>
+				<br>
+				<?php echo form_error('contrasenia') ?>
+				<br>
 
-<?php echo form_submit($submit);?>
-<?php echo form_close();?>
+				<?php echo form_submit($submit);?>
+				<?php echo form_close();?>
 
-					<div class="copy-text">
-          <h5>
-            <a class="d-block small mt-3" href="<?php echo base_url('index.php/login/Registrar')?>">Registrarme</a>
-          </h5>
-          <h5>
-            <a class="d-block small " href="forgot-password.html">¿Olvidaste tu clave?</a>
-          </h5>
-				</div>
-			</div>
-			<div class="col-md-8 banner-sec">
-				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-						
-					</ol>
-					<div class="carousel-inner" role="listbox">
-						<div class="carousel-item active">
-							<img class="img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
-							<div class="carousel-caption d-none d-md-block">
-								<div class="banner-text">
-									<h2>Bienvenido/a</h2>
-									<p>Regístrate y disfruta de nuestros servicios.</p>
-								</div>
-							</div>
-						</div>
-						
-						
+				<div class="copy-text">
+					<div class="registrarme">
+						<a class="" href="<?php echo base_url('index.php/login/Registrar')?>">Registrarme</a>
 					</div>
-
+						
+					<div class="forgot">
+						<a class=" " href="forgot-password.html">¿Olvidaste tu clave?</a>
+					</div>
+					
+						
+					
 				</div>
 			</div>
+
 		</div>
 </section>
 
@@ -107,7 +97,7 @@ $submit = array(
 	}
 
 	.banner-sec {
-		background: url(https://static.pexels.com/photos/33972/pexels-photo.jpg) no-repeat left bottom;
+		height: 500px;
 		background-size: cover;
 		min-height: 500px;
 		border-radius: 0 10px 10px 0;
@@ -120,14 +110,7 @@ $submit = array(
 		box-shadow: 15px 20px 0px rgba(0, 0, 0, 0.1);
 	}
 
-	.carousel-inner {
-		border-radius: 0 10px 10px 0;
-	}
 
-	.carousel-caption {
-		text-align: left;
-		left: 5%;
-	}
 
 	.login-sec {
 		padding: 50px 30px;
@@ -175,47 +158,19 @@ $submit = array(
 		font-weight: 600;
 	}
 
-	.banner-text {
-		width: 70%;
-		position: absolute;
-		bottom: 40px;
-		padding-left: 20px;
+
+	section {
+		margin-bottom: 20px;
 	}
 
-	.banner-text h2 {
-		color: #fff;
-		font-weight: 600;
+	button[type="submit"] {
+		margin-top: 10px;
+	}
+	.registrarme, .forgot{
+		text-align: left;
+	}
+	.registrarme a,.forgot a{
+		font-size: 20px;
 	}
 
-	.banner-text h2:after {
-		content: " ";
-		width: 100px;
-		height: 5px;
-		background: #FFF;
-		display: block;
-		margin-top: 20px;
-		border-radius: 3px;
-	}
-
-	.banner-text p {
-		color: #fff;
-	}
-  section{
-    margin-bottom: 40px;
-  }
-  button[type="submit"]{
-    margin-top: 10px;
-  }
-  .l > p {
-    color: black;
-    font-weight: bold;
-  }
-.img-fluid{
-	height: 500px;
-}
-@media only screen and (max-width: 600px) {
-   .banner-sec  {
-        display: none;
-    }
-}
 </style>
